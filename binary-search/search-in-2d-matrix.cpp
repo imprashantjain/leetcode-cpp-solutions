e37dce48c5fc012682524bc089ea66c8e3892c1c
imprashantjain <Prashantjain101202@gmail.com>
2025-08-07 12:22:29 +0530

    Add searchMatrix solution with main function

---
+++ binary-search/search-in-2d-matrix.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if (matrix[row][col] < target) {
+                row++;
+            }
+            else {
+                col--;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+
+    int target = 3;
+
+    if (sol.searchMatrix(matrix, target)) {
+        cout << "Target found!" << endl;
+    } else {
+        cout << "Target not found." << endl;
+    }
+
+    return 0;
+}
